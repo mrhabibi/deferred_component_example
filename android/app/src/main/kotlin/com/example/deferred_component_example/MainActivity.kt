be8434b468df5ef2e45666e2baceda349faa26f1
Muhammad Rizky Habibi <habibi@bukalapak.com>
2024-04-01 10:06:38 +0700

    up flutter

--- android/app/src/main/kotlin/com/example/deferred_component_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deferred_component_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.deferred_component_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
